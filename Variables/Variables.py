4fd666db514dc1e290416ede66f8b4bb5bfabf58
Abhishek Kumar <avirr058@gmail.com>
2025-03-03 13:42:27 +0530

    Add files via upload

---
+++ Variables/Variables.py
@@ -0,0 +1,18 @@
+#Understanding variables and how to name them
+a = "hello"
+print (a)
+
+A = "hello case sensitive"
+print(A)
+
+b="B here is a variable"
+print (b)
+
+myBaskt = "hundred"
+print(myBaskt)
+
+my_Basket = "hundred one"
+print(my_Basket)
+
+Num1 = "hello Num1"
+print(Num1)
