4b68a4d873785d00df63b3b4750916070cf44283
Abhishek Kumar <avirr058@gmail.com>
2025-03-03 13:46:15 +0530

    Update print and cmnts.py

--- Print and Comments/print and cmnts.py
+++ Print and Comments/print and cmnts.py
@@ -1,1 +1,10 @@
+print ("hello world")
+print("this is first program")
 
+print("trying double line print \nhello there")
+print("""hello i am Abhishek
+this is my first program""")
+
+#this is single line comment.
+"""this is multiple line comment
+here is multiple lines"""
