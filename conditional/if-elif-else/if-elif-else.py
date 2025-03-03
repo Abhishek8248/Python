7c2e71fa57e8974fc1a363b2bfdcd0a9f7df0415
Abhishek Kumar <avirr058@gmail.com>
2025-03-03 13:58:15 +0530

    Create if-elif-else.py

---
+++ conditional/if-elif-else/if-elif-else.py
@@ -0,0 +1,13 @@
+marks = int(input("Enter your Marks:"))
+
+if marks >=90:
+    print("You got grade A+")
+
+elif marks >70 and marks<=89:
+    print("You got grade A")
+
+elif marks >= 50 and marks <= 69:
+    print("You got grade B")
+
+else:
+    print("You failed!")
