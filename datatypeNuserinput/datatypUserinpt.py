5bee632fb70ac44f1c6941881f92efe1925f7760
Abhishek Kumar <avirr058@gmail.com>
2025-03-03 13:55:49 +0530

    Create datatypUserinpt.py

---
+++ datatypeNuserinput/datatypUserinpt.py
@@ -0,0 +1,6 @@
+name = input("enter your name:")
+age =  int(input("Enter your age:"))
+height = float(input("Enter your height:"))
+
+c=eval(input("Enter equation here:"))
+print(name,age, height, c)
