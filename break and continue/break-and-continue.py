a33302a0e7a2e0a996611dae91927b07a9a5bb3d
Abhishek Kumar <avirr058@gmail.com>
2025-03-08 13:11:14 +0530

    Create break-and-continue.py

---
+++ break and continue/break-and-continue.py
@@ -0,0 +1,16 @@
+#continue-statement
+print("Continue Statement")
+
+for i in range(1,11):
+    if i==5:
+        continue
+    else:
+        print(i)
+print("Break Statement")
+for i in range (1,11):
+    if i == 7:
+        break
+    else:
+        print(i)
+
+print("thank you")
