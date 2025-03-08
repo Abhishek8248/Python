7f566647c595a52e30ef0b59bb241b2a3da46fc0
Abhishek Kumar <avirr058@gmail.com>
2025-03-08 13:10:08 +0530

    Add files via upload

---
+++ loops/for-loops-with-condStatement.py
@@ -0,0 +1,13 @@
+n = 1
+
+while n <= 10:
+    if n == 3:
+        print("added")
+    else:
+        print(n)
+
+    n += 1
+#-------------------------------
+for i in range(1,101):
+    if i%8==0 and i%12==0:
+        print(i)
